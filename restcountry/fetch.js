563f07d4e2ddef9e2e97fecddf4c154bcbd2af9d
sanjay892000 <sanjay892000@gmail.com>
2024-10-10 14:44:04 +0530

    selfrestcountry

---
+++ restcountry/fetch.js
@@ -0,0 +1,39 @@
+let country = []
+let fetchFun = async () => {
+   try {
+    let response = await fetch("https://restcountries.com/v3.1/all");
+    let data = await response.json()
+    data.forEach(element => {
+        country.push(element)
+        displayCountry(element)
+    });
+   } catch (error) {
+    console.log(error)
+   }
+}
+document.body.addEventListener('onload', fetchFun())
+let container = document.querySelector('.container');
+function displayCountry(value) {
+        let div = document.createElement('div');
+        div.setAttribute('class','card');
+        div.innerHTML = `<div><img src=${value.flags.png} alt="loading..."></div>
+        <h1>${value.name.common}</h1>
+        <p>Population: <span>${value.population}</span></p>
+        <p>Region: <span>${value.region}</span></p>
+        <p>Capital: <span>${value.capital}</span></p>`
+         container.appendChild(div)
+}
+
+document.querySelector('.inputdata').addEventListener('input', (e) => {
+    let searchData = e.target.value.toLowerCase();
+    let search = country.filter(filterFun);
+    function filterFun(val) {
+        return val.name.common.toLowerCase().includes(searchData)
+    }
+    console.log(search)
+    document.querySelector('.container').innerHTML = " "
+    search.forEach((val)=>{
+        displayCountry(val)
+    })
+});
+
