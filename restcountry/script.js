79076cb4f507a41462db96e6308efbbadbca0d09
sanjay892000 <sanjay892000@gmail.com>
2024-10-12 02:03:17 +0530

    restcountryself

--- restcountry/script.js
+++ restcountry/script.js
@@ -1,33 +1,34 @@
 let country = []
 let fetchFun = async () => {
-   try {
-    let response = await fetch("https://restcountries.com/v3.1/all");
-    let data = await response.json()
-    data.forEach(element => {
-        country.push(element)
-    });
-    displayCountry(data)
-   } catch (error) {
-    console.log(error)
-   }
+    try {
+        let response = await fetch("https://restcountries.com/v3.1/all");
+        let data = await response.json()
+        data.forEach(element => {
+            country.push(element)
+        });
+        displayCountry(data)
+    } catch (error) {
+        console.log(error)
+    }
 }
 document.body.addEventListener('onload', fetchFun())
 let container = document.querySelector('.container');
 function displayCountry(countryData) {
-       countryData.map((value)=>{
+    document.querySelector('.container').innerHTML = " ";
+    countryData.map((value) => {
         let div = document.createElement('div');
-        div.setAttribute('class','card');
+        div.setAttribute('class', 'card');
         div.innerHTML = `<div><img src=${value.flags.png} alt="loading..."></div>
         <h1>${value.name.common}</h1>
         <p>Population: <span>${value.population}</span></p>
         <p>Region: <span>${value.region}</span></p>
         <p>Capital: <span>${value.capital}</span></p>`
-         container.appendChild(div)
-       });
+        container.appendChild(div)
+    });
 }
 
 document.querySelector('.inputdata').addEventListener('input', (e) => {
-    let searchData = e.target.value.toLowerCase();
+    let searchData = e.target.value.toLowerCase().trim();
     let search = country.filter(filterFun);
     function filterFun(val) {
         return val.name.common.toLowerCase().includes(searchData)
@@ -37,3 +38,51 @@ document.querySelector('.inputdata').addEventListener('input', (e) => {
     displayCountry(search)
 });
 
+
+document.querySelector('.filter').addEventListener('click', () => {
+    let filter = document.querySelector('.filterlist')
+    filter.style.display == "block" ? filter.style.display = "none" : filter.style.display = "block";
+})
+
+
+let regionFun = async (region) => {
+    let response = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+    let resgionData = await response.json();
+    document.querySelector('.container').innerHTML = " "
+    displayCountry(resgionData)
+}
+
+let liList = document.querySelectorAll('.region');
+
+liList.forEach(li => {
+
+    li.addEventListener('click', () => {
+        document.querySelector('.inputdata').value = ""
+        regionFun(li.textContent)
+        liList.forEach(item => {
+            item.classList.remove('active')
+        })
+        li.classList.add('active')
+
+    })
+})
+
+
+document.querySelector('.changemode').addEventListener('click', () => {
+    if (document.body.classList == "light") {
+        document.body.classList.remove("light")
+        document.body.classList.add("dark")
+    }
+    else {
+        document.body.classList.remove("dark")
+        document.body.classList.add("light")
+    }
+})
+
+
+document.querySelector('.toparrow').addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+})
