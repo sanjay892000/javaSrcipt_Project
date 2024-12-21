17bd16a17564dc4717893c67b279b91519f98e6b
sanjay892000 <sanjay892000@gmail.com>
2024-12-21 14:58:46 +0530

    carousel add

--- carousel/script.js
+++ carousel/script.js
@@ -1,13 +1,42 @@
 let carouselItems = document.querySelectorAll('.carousel-items')
-console.log(carouselItems)
 
 let carouselSlider = 0;
-carouselItems.forEach((items, index)=>{
-    items.style.left=`${index * 100}%`
+carouselItems.forEach((items, index) => {
+    items.style.left = `${index * 100}%`
 })
 
-/* const carouselFun = (carouselSlider)=>{
-    carouselItems.forEach((items)=>{
+const carouselFun = (carouselSlider) => {
+    carouselItems.forEach((items) => {
         items.style.transform = `translateX(-${carouselSlider * 100}%)`
     })
-} */
+}
+
+let prevBtn = document.querySelector('.prevBtn');
+let nextBtn = document.querySelector('.nextBtn');
+
+prevBtn.addEventListener('click', () => {
+    carouselSlider--;
+    if (carouselSlider < 0) {
+        carouselSlider = carouselItems.length - 1;
+    }
+    carouselFun(carouselSlider);
+    console.log(carouselSlider);
+    })
+    
+nextBtn.addEventListener('click', () => {
+    carouselSlider++;
+    if (carouselSlider > carouselItems.length - 1) {
+        carouselSlider = 0;
+    }
+    carouselFun(carouselSlider);
+    console.log(carouselSlider);
+})
+
+/* setInterval(() => {
+    carouselSlider++;
+    if (carouselSlider > carouselItems.length - 1) {
+        carouselSlider = 0;
+    }
+    carouselFun(carouselSlider);
+}, 3000); */
+
