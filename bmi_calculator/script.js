1847896d38e89a8d41e7588af03f29c93f9cf8b0
sanjay892000 <sanjay892000@gmail.com>
2024-12-22 23:10:46 +0530

    bmi calculator layout change

--- bmi_calculator/script.js
+++ bmi_calculator/script.js
@@ -3,14 +3,31 @@ let weight = document.querySelector('#weight'); */
 let btn = document.querySelector('.btn-primary');
 let form = document.querySelector('form')
 
-form.addEventListener('submit',calculateBMI);
+form.addEventListener('submit', calculateBMI);
 
-function calculateBMI(event){
+function calculateBMI(event) {
     event.preventDefault();
     let height = form[0].value;
-    hInMet = height/100;
+    hInMet = height / 100;
     let weight = form[1].value;
-    let valBMI = weight/(hInMet*2)
+    let valBMI = weight / (hInMet * 2)
     let result = document.querySelector('.result');
-    result.innerHTML=valBMI.toFixed(2);
+
+    if (valBMI < 18) {
+        result.style.color = 'red';
+        result.innerHTML = `BMI Value is: ${valBMI.toFixed(2)}, You are Underweight`;
+    }
+    else if (valBMI <= 24) {
+        result.style.color = 'green';
+        result.innerHTML = `BMI Value is: ${valBMI.toFixed(2)}, You are Normal Weight`;
+    }
+    else if (valBMI <= 30) {
+        result.style.color = 'orange';
+        result.innerHTML = `BMI Value is: ${valBMI.toFixed(2)}, You are Overweight`;
+    }
+    else {
+        result.style.color ='red';
+        result.innerHTML = `BMI Value is: ${valBMI.toFixed(2)}, You are Obese`;
+    }
+
 }
