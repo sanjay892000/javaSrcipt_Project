5c3552e0640fc67af55fbd357cd79f039b72bfec
sanjay892000 <sanjay892000@gmail.com>
2024-12-24 15:22:00 +0530

    localstorage add

--- verification_with_ans/script.js
+++ verification_with_ans/script.js
@@ -5,50 +5,45 @@ let form = document.querySelector('#myform');
 
 
 scoreTag.innerHTML = score;
-let valueA;
-let valueB;
-let taskAns;
-let symbol; 
 const questionFun = () =>{
     
-valueA = Math.floor(Math.random() * 10 + 1);
-valueB = Math.floor(Math.random() * 10 + 1);
+let valueA = Math.floor(Math.random() * 10 + 1);
+let valueB = Math.floor(Math.random() * 10 + 1);
 
 //operation value
 let sum = valueA + valueB;
 let subtract;
 if (valueA < valueB) {
+    let revData = valueB
     valueB = valueA;
-    valueB = valueB;
+    valueA = revData;
 }
 subtract = valueA - valueB;
-
 let mul = valueA * valueB;
-
 const result = [sum, subtract, mul];
 let operator = ['+', '-', '*'];
 let task = Math.floor(Math.random() * operator.length);
-taskAns = result[task];
-symbol = operator[task];
+localStorage.setItem('taskAns', result[task]);
+let symbol = operator[task];
 console.log(valueA, valueB);
-console.log(taskAns, symbol);
 let questions = document.querySelector('.questions')
 questions.innerHTML = `what is answer of ${valueA} ${symbol} ${valueB} ?`;
 }
 
 window.addEventListener('load', questionFun)
-
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     let inputData = Number(document.querySelector('#inputData').value);
-    if(inputData == taskAns){
+    if(inputData == localStorage.getItem('taskAns')){
         score+=1;
         scoreTag.innerHTML = score;
+        localStorage.removeItem('taskAns');
         document.querySelector('#inputData').value = ""
     }
     else{
         score-=1;
         scoreTag.innerHTML = score;
+        localStorage.removeItem('taskAns');
         document.querySelector('#inputData').value = ""
     }
     questionFun()
