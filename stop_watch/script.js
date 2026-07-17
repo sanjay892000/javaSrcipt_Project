d530f21b3bc5396138c104c2acc39022aaa0bf77
sanjay892000 <sanjay892000@gmail.com>
2026-07-17 18:56:00 +0530

    change the functionality

--- stop_watch/script.js
+++ stop_watch/script.js
@@ -1,71 +1,69 @@
+let hour = 0
+let minute = 0
+let second = 0
 let clock = document.querySelector('.clock');
-let snd = 0;
-let mnt = 0;
-let hur = 0;
+clock.innerHTML = `${hour.toString().padStart(2, "0")} : ${minute.toString().padStart(2, "0")} : ${second.toString().padStart(2, "0")}`
+function StartStopWatch() {
+    second++;
+
+    if (second == 60) {
+        second = 0
+        minute++
+    }
+    if (minute == 60) {
+        minute = 0
+        hour++
+    }
+
+    console.log(`${hour} : ${minute} : ${second}`)
+    clock.innerHTML = `${hour.toString().padStart(2, "0")} : ${minute.toString().padStart(2, "0")} : ${second.toString().padStart(2, "0")}`
+}
+
+/* setInterval(StartStopWatch, 1000) */
 
 
-let second = snd.toString().padStart(2, '0')
-let minute = mnt.toString().padStart(2, '0')
-let hour = hur.toString().padStart(2, '0')
-clock.innerHTML = `${hour}:${minute}:${second}`
-let interval;
+const startBtn = document.querySelector('.start');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
 
-let startBtn = document.querySelector('.start');
-let stopBtn = document.querySelector('.stop')
-let resetBtn = document.querySelector('.reset')
-stopBtn.disabled = true;
-resetBtn.disabled = true;
-
-
-
+stopBtn.disabled = true
+resetBtn.disabled = true
+let refTime;
 startBtn.addEventListener('click', () => {
-    interval = setInterval(function () {
+    refTime = setInterval(StartStopWatch, 1000)
 
-        second++
-        if (second == 60) {
-            second = 0
-            minute++
-        }
-        if (minute == 60) {
-            minute = 0
-            hour++
-        }
-        if (hour == 24) {
-            hour = 0;
-        }
-
-        clock.innerHTML = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${second.toString().padStart(2, '0')}`
-    }, 1000)
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
-    resetBtn.disabled = false;
+    startBtn.disabled = true
+    stopBtn.disabled = false
+    resetBtn.disabled = false
 })
-
-
 
 stopBtn.addEventListener('click', () => {
-    clearInterval(interval)
-    stopBtn.disabled = true;
-    startBtn.disabled = false;
+    clearInterval(refTime)
+
+    startBtn.disabled = false
+    stopBtn.disabled = true
+    resetBtn.disabled = false
 })
 
-resetBtn.addEventListener('click', ()=>{
-    clearInterval(interval)
-    clock.innerHTML = `00:00:00`;
-    startBtn.disabled = false;
-    stopBtn.disabled = true;
-    resetBtn.disabled = true;
-})
-let date = document.querySelector('.date');
-let myDate = new Date();
-date.innerHTML = `${myDate.getDate()}/${myDate.getMonth()+1}/${myDate.getFullYear()}`
+resetBtn.addEventListener('click', () => {
+    clearInterval(refTime)
+    second = 0
+    minute = 0
+    hour = 0
 
-/* 
-console.log(myDate.toString())
-console.log(myDate.toISOString())
-console.log(myDate.toJSON())
-console.log(myDate.toLocaleString())
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toTimeString())
-console.log(myDate.toLocaleTimeString())
-console.log(myDate.toDateString()) */
+    clock.innerHTML = `${hour.toString().padStart(2, "0")} : ${minute.toString().padStart(2, "0")} : ${second.toString().padStart(2, "0")}`
+
+    startBtn.disabled = false
+    stopBtn.disabled = true
+    resetBtn.disabled = true
+})
+
+
+let date = new Date()
+const dateElm = document.querySelector('.date')
+dateElm.innerHTML = date.toLocaleDateString();
+
+setInterval(() => {
+    date = new Date()
+    dateElm.innerHTML = date.toLocaleDateString();
+}, 1000)
